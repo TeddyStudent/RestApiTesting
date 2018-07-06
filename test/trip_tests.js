17b426c1a027173d4d85697319cc3568cbdfc7ce
TeddyStudent <teukka@bastu.net>
2018-07-06 12:44:46 +0300

    Test cases for Trip added

--- test/trip_tests.js
+++ test/trip_tests.js
@@ -8,56 +8,128 @@ var newtripid;
 
 describe('Trip', function () {
 
-    it('should return a 200 response', function (done) {
+    it('should be an object with keys and values', function (done) {
         api.get('/api/trip/1')
             .set('Accept', 'application/json')
-            .expect(200, done);
-    });
-
-    /*
-    it('should be successful user creation with 201 response', function (done) {
-        api.post('/api/trip')
-            .set('Accept', 'application/json')
-            .send({
-                etunimi: "USER123",
-                sukunimi: "TEST123",
-                kayttajatunnus: "TEST123ID",
-                salasana: "TEST123PWD"
-            })
-            //.expect(201,done);
-            .expect(201)
+            .expect(200)
             .end(function (err, res) {
-                expect(res.body[0]).to.have.property("idtili");
-                expect(res.body[0].idtili).to.not.equal(null);
-                newtripid = res.body[0].idtili;
-                expect(res.body[0]).to.have.property("etunimi");
-                expect(res.body[0].etunimi).to.not.equal(null);
-                expect(res.body[0]).to.have.property("sukunimi");
-                expect(res.body[0].sukunimi).to.not.equal(null);
-                expect(res.body[0]).to.have.property("kayttajatunnus");
-                expect(res.body[0].kayttajatunnus).to.not.equal(null);
-                expect(res.body[0]).to.have.property("salasana");
-                expect(res.body[0].salasana).to.not.equal(null);
+                expect(res.body[0]).to.have.property("idreissu");
+                expect(res.body[0].idreissu).to.not.equal(null);
+                expect(res.body[0]).to.have.property("pvm");
+                expect(res.body[0].pvm).to.not.equal(null);
+                expect(res.body[0]).to.have.property("paikka");
+                expect(res.body[0].paikka).to.not.equal(null);
+                expect(res.body[0]).to.have.property("saa");
+                expect(res.body[0].saa).to.not.equal(null);
+                expect(res.body[0]).to.have.property("t_nopeus");
+                expect(res.body[0].t_nopeus).to.not.equal(null);
+                expect(res.body[0]).to.have.property("t_suunta");
+                expect(res.body[0].t_suunta).to.not.equal(null);
+                expect(res.body[0]).to.have.property("l_ilma");
+                expect(res.body[0].l_ilma).to.not.equal(null);
+                expect(res.body[0]).to.have.property("l_vesi");
+                expect(res.body[0].l_vesi).to.not.equal(null);
+                expect(res.body[0]).to.have.property("tili_idtili");
+                expect(res.body[0].tili_idtili).to.not.equal(null);
                 done();
             });
     });
-
+    
+    it('should be successful trip creation with 201 response', function (done) {
+        api.post('/api/trip')
+            .set('Accept', 'application/json')
+            .send({
+                pvm: "2018-07-06",
+                paikka: "Pyhäjärvi",
+                saa: "pilvinen",
+                t_nopeus: "1",
+                t_suunta: "kaakko",
+                l_ilma: "22",
+                l_vesi: "18",
+                tili_idtili: "1"
+            })
+            .expect(201)
+            .end(function (err, res) {
+                expect(res.body[0]).to.have.property("idreissu");
+                expect(res.body[0].idreissu).to.not.equal(null);
+                newtripid = res.body[0].idreissu;
+                expect(res.body[0]).to.have.property("pvm");
+                expect(res.body[0].pvm).to.not.equal(null);
+                expect(res.body[0]).to.have.property("paikka");
+                expect(res.body[0].paikka).to.not.equal(null);
+                expect(res.body[0]).to.have.property("saa");
+                expect(res.body[0].saa).to.not.equal(null);
+                expect(res.body[0]).to.have.property("t_nopeus");
+                expect(res.body[0].t_nopeus).to.not.equal(null);
+                expect(res.body[0]).to.have.property("t_suunta");
+                expect(res.body[0].t_suunta).to.not.equal(null);
+                expect(res.body[0]).to.have.property("l_ilma");
+                expect(res.body[0].l_ilma).to.not.equal(null);
+                expect(res.body[0]).to.have.property("l_vesi");
+                expect(res.body[0].l_vesi).to.not.equal(null);
+                expect(res.body[0]).to.have.property("tili_idtili");
+                expect(res.body[0].tili_idtili).to.not.equal(null);
+                done();
+            });
+    });
+    
     it('should be updated with a new wind speed (t_nopeus)', function (done) {
         api.put('/api/trip/' + newtripid)
             .set('Accept', 'application/json')
             .send({
-                etunimi: "User11",
-                sukunimi: "Surname1",
-                kayttajatunnus: "user1id",
-                salasana: "user1pwd"
+                pvm: "2018-07-06",
+                paikka: "Pyhäjärvi",
+                saa: "pilvinen",
+                t_nopeus: "2",
+                t_suunta: "kaakko",
+                l_ilma: "22",
+                l_vesi: "18",
+                tili_idtili: "1"
             })
-            .expect(200,'[{"idtili":2,"etunimi":"User11","sukunimi":"Surname1","kayttajatunnus":"user1id","salasana":"user1pwd"}]',done);
-    });    
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body[0]).to.have.property("idreissu");
+                expect(res.body[0].idreissu).to.not.equal(null);
+                expect(res.body[0]).to.have.property("pvm");
+                expect(res.body[0].pvm).to.not.equal(null);
+                expect(res.body[0]).to.have.property("paikka");
+                expect(res.body[0].paikka).to.not.equal(null);
+                expect(res.body[0]).to.have.property("saa");
+                expect(res.body[0].saa).to.not.equal(null);
+                expect(res.body[0]).to.have.property("t_nopeus");
+                expect(res.body[0].t_nopeus).to.equal(2);
+                expect(res.body[0]).to.have.property("t_suunta");
+                expect(res.body[0].t_suunta).to.not.equal(null);
+                expect(res.body[0]).to.have.property("l_ilma");
+                expect(res.body[0].l_ilma).to.not.equal(null);
+                expect(res.body[0]).to.have.property("l_vesi");
+                expect(res.body[0].l_vesi).to.not.equal(null);
+                expect(res.body[0]).to.have.property("tili_idtili");
+                expect(res.body[0].tili_idtili).to.not.equal(null);
+                done();
+            });
+    });   
+    
+    it('should be failed modification with 400 response', function (done) {
+        api.put('/api/trip/' + newtripid)
+            .set('Accept', 'application/json')
+            .send({
+                pvm: "2018-07-06",
+                paikka: "toolongplacenametoolongplacenametoolongplacename",
+                saa: "pilvinen",
+                t_nopeus: "2",
+                t_suunta: "kaakko",
+                l_ilma: "22",
+                l_vesi: "18",
+                tili_idtili: "1"
+            })
+            .expect(400,done);
+    }); 
 
     it('should be successful trip deletion with 200 response', function (done) {
         api.delete('/api/trip/' + newtripid)
             .set('Accept', 'application/json')
             .expect(200,done);
     });
-    */
+
 });
